794de72dca96df86e7eafb73a6671b22940d83df
mohgavin <67898944+mohgavin@users.noreply.github.com>
2022-09-29 08:23:03 +0700

    Update enterkomputer_low.py

--- enterkomputer_low.py
+++ enterkomputer_low.py
@@ -4,8 +4,8 @@ from initialization import loading_website, regex1, regex2, table_process1, tabl
 from telethon import TelegramClient
 
 # Remember to use your own values from my.telegram.org!
-api_id = #Di isi dengan API ID sendiri
-api_hash = #Di isi dnegan API ID sendiri
+api_id = 8127938 #Di isi dengan API ID sendiri
+api_hash = 'uhasgdasd123kuasg' #Di isi dnegan API ID sendiri
 client = TelegramClient('/home/nivag/enterkomputer_scrap/nivagad', api_id, api_hash)
 
 async def main():
